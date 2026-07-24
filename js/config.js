506e1a55d709e67fa11b6c2dce83200b4aa19002
humanparkhyorim-alt <humanparkhyorim@gmail.com>
2026-07-24 16:52:29 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // ✅ Apps Script 배포 후 여기에 URL을 붙여넣으세요
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw-K87YA9f2HhUcuviznfsM0izL27JS2F8T3avcrMoktlUv6yyZM_sylEnirYJuMSVC2g/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyd6PEUmWgQtYgWfa5rps4WHqR0TSXIE5Z9wXWNaqvERn8IjsBKLHK7kSxBysPsvWgBBw/exec";
